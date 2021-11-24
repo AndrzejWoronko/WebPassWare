82695520b3818a5661e3e02e3cb4955dcd1edb20
Andrzej Woronko <andrzej.woronko@gmail.com>
2021-11-24 16:20:52 +0100

    Poprawka skryptu komilacji macos

--- scripts/macos/compile.sh
+++ scripts/macos/compile.sh
@@ -2,16 +2,11 @@
 
 set -x
 
-QMAKE="~/Qt/5.15.0/clang_64/bin/qmake"
+QMAKE="~/Qt/5.15.2/clang_64/bin/qmake"
 
 cdir=`pwd`
 cpu_cores=`sysctl -n hw.logicalcpu`
-absolute_path=`realpath $0`
-this_dir=`dirname $absolute_path`
-this_dir=`dirname $this_dir`
-parent_dir=`dirname $this_dir`
 
-cd $parent_dir
 mkdir -p output/build/bin/
 
 cd output/build/
